f9f4b87fc9dc3f034f1f82d3454093264f36c182
Justin2434 <kaizonheilrd@gmail.com>
2024-11-14 16:52:23 +0800

    done all one bug on banner slider

---
+++ dist/js/aboutBannerSlider.js
@@ -0,0 +1,22 @@
+var slider = tns({
+    container: '.menu_tabs--mobile ul',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    mouseDrag: true,
+    loop: false,
+    controls: false,
+    navPosition: "bottom",
+  });
+
+
+// var sectext1 = tns({
+//     container: '.sec1__text.active',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     mouseDrag: true,
+//     controls: false,
+//     loop: false,
+//     navPosition: "bottom",
+//   });
---
+++ dist/js/commitment_slider.js
@@ -0,0 +1,10 @@
+var slider = tns({
+    container: '.sec3__items--mobile',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    mouseDrag: true,
+    loop: false,
+    controls: false,
+    navPosition: "bottom",
+  });
